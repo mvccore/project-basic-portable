5ef104badebebee445ca85ef4e45271e17eddce7
Tom Flídr <tomflidr@gmail.com>
2019-01-09 11:52:07 +0100

    new packaging with static files copying functionality

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -13,9 +13,10 @@ class Bootstrap
 		}
 		
 		/**
-		 * Use this line only if you want to pack application without JS/CSS/fonts/images
-		 * inside package and you want to have all those files placed on hard drive manually.
-		 * You can use this variant in modes PHP_PRESERVE_PACKAGE, PHP_PRESERVE_HDD and PHP_STRICT_HDD
+		 * Uncomment this line before generate any assets into temporary directory, before application 
+		 * packing/building, only if you want to pack application without JS/CSS/fonts/images inside
+		 * result PHP package and you want to have all those files placed on hard drive.
+		 * You can use this variant in modes `PHP_PRESERVE_PACKAGE`, `PHP_PRESERVE_HDD` and `PHP_STRICT_HDD`.
 		 */
 		//\MvcCore\Ext\Views\Helpers\Assets::SetAssetUrlCompletion(FALSE);
 
