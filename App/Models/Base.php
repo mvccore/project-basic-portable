35a6d161fb9c69d88ae1fe03dd294fba717cbdd2
Tom Flídr <tomflidr@gmail.com>
2023-05-17 14:18:06 +0200

    Base model refactoring.

--- App/Models/Base.php
+++ App/Models/Base.php
@@ -4,24 +4,21 @@ namespace App\Models;
 
 class Base extends \MvcCore\Model {
 
-	/**
-	 * System config connection index.
-	 * @var int
-	 */
-	protected static $connectionName = 0;
-
 	public static function GetAllDbTables () {
 		$dbName = self::GetConfig()->database;
 
-		$select = self::GetConnection()->prepare(implode("\n", [
-			"SELECT	TABLE_NAME as TableName	",
-			"FROM information_schema.TABLES	",
-			"WHERE TABLE_SCHEMA = :dbName	",
-		]));
-		$select->execute(['dbName' => $dbName]);
+		$select = self::GetConnection()->prepare("
+			SELECT
+				TABLE_NAME as TableName
+			FROM
+				information_schema.TABLES
+			WHERE
+				TABLE_SCHEMA = :dbName
+		");
+		$select->execute([':dbName' => $dbName]);
 
 		$rawResult = $select->fetchAll(\PDO::FETCH_ASSOC);
-
+		
 		$result = [];
 		foreach ($rawResult as $item)
 			$result[] = $item['TableName'];
